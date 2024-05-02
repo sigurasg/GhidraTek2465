5e76dea902480971c5d18be1e1041dd5e4ed5338
Sigurdur Asgeirsson <siggi@undo.com>
2024-05-02 16:08:30 -0400

    Add build workflows, move java modules to is/sort.

--- src/main/is/sort/GhidraTek2465/Tek2465Loader.java
+++ src/main/is/sort/GhidraTek2465/Tek2465Loader.java
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-package tek2465;
+package is.sort.GhidraTek2465;
 
 import static ghidra.program.model.data.DataUtilities.createData;
 
