df9bc555ceb53f400f844515195a30435094248e
Sigurdur Asgeirsson <siggi@undo.com>
2024-05-07 12:23:54 -0400

    Add missing test file.

---
+++ src/test/java/is/sort/GhidraTek2465/ROMUtilsTest.java
@@ -0,0 +1,38 @@
+// Copyright 2024 Sigurdur Asgeirsson <siggi@sort.is>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package is.sort.GhidraTek2465;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ROMUtilsTest {
+	@Test
+	public void getScopeKindTest() {
+		assertEquals(ROMUtils.scopeKindFromPartNumber(1625), ROMUtils.ScopeKind.TEK2465);
+		assertEquals(ROMUtils.scopeKindFromPartNumber(1626), ROMUtils.ScopeKind.TEK2465);
+		assertEquals(ROMUtils.scopeKindFromPartNumber(1627), ROMUtils.ScopeKind.TEK2465);
+		assertEquals(ROMUtils.scopeKindFromPartNumber(1628), ROMUtils.ScopeKind.TEK2465);
+
+		assertEquals(ROMUtils.scopeKindFromPartNumber(3302), ROMUtils.ScopeKind.TEK2465A);
+		assertEquals(ROMUtils.scopeKindFromPartNumber(3303), ROMUtils.ScopeKind.TEK2465A);
+
+		assertEquals(ROMUtils.scopeKindFromPartNumber(5370), ROMUtils.ScopeKind.TEK2465B);
+		assertEquals(ROMUtils.scopeKindFromPartNumber(5371), ROMUtils.ScopeKind.TEK2465B);
+
+		assertEquals(ROMUtils.scopeKindFromPartNumber(5876), ROMUtils.ScopeKind.TEK2465B_LATE);
+		assertEquals(ROMUtils.scopeKindFromPartNumber(5877), ROMUtils.ScopeKind.TEK2465B_LATE);
+	}
+}
