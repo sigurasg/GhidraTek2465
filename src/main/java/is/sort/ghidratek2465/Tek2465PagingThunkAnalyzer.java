d2aa42054a49f6c8d0a1a1539139300d0fbdd216
Sigurdur Asgeirsson <siggi@undo.com>
2024-05-31 12:52:15 -0400

    Now caters for B-type thunks and runs iteratively.

--- src/main/java/is/sort/ghidratek2465/Tek2465PagingThunkAnalyzer.java
+++ src/main/java/is/sort/ghidratek2465/Tek2465PagingThunkAnalyzer.java
@@ -13,9 +13,14 @@
 // limitations under the License.
 package is.sort.ghidratek2465;
 
+import java.io.IOException;
+
 import ghidra.app.cmd.disassemble.DisassembleCommand;
+import ghidra.app.plugin.core.analysis.AutoAnalysisManager;
 import ghidra.app.services.AbstractAnalyzer;
 import ghidra.app.services.AnalyzerType;
+import ghidra.app.util.bin.ByteProvider;
+import ghidra.app.util.bin.MemoryByteProvider;
 import ghidra.app.util.importer.MessageLog;
 import ghidra.framework.options.Options;
 import ghidra.program.database.function.OverlappingFunctionException;
@@ -52,6 +57,7 @@ import ghidra.util.task.TaskMonitor;
  * the destination page.
  */
 public class Tek2465PagingThunkAnalyzer extends AbstractAnalyzer {
+	private static final String OPTION_SCOPE_KIND = "Scope kind";
 	private static final String OPTION_REPROCESS_THUNKS = "Reprocess existing thunks";
 	private static final String OPTION_MARK_PAGE_CALLER_FUNCTIONS =
 		"Mark all callers of PAGE_* as functions";
@@ -72,15 +78,22 @@ public class Tek2465PagingThunkAnalyzer extends AbstractAnalyzer {
 
 	@Override
 	public boolean canAnalyze(Program program) {
-		if (program.getLanguageID().equals(new LanguageID("MC6800:BE:16:default"))) {
-			return true;
+		if (!program.getLanguageID().equals(new LanguageID("MC6800:BE:16:default"))) {
+			return false;
+		}
+		scopeKind = getScopeKindFromProgram(program);
+		if (scopeKind == ScopeKind.UNKNOWN || scopeKind == ScopeKind.TEK2465) {
+			// There's no paging on the 2465.
+			return false;
 		}
 
-		return false;
+		return true;
 	}
 
 	@Override
 	public void registerOptions(Options options, Program program) {
+		// TODO(siggi): Should the scopeKind come from program here?
+		options.registerOption(OPTION_SCOPE_KIND, scopeKind, null, "Scope Kind.");
 		options.registerOption(OPTION_REPROCESS_THUNKS, reprocessThunks, null,
 			"Reprocess existing thunks.");
 		options.registerOption(OPTION_MARK_PAGE_CALLER_FUNCTIONS, markPageCallersAsFunctions, null,
@@ -89,6 +102,7 @@ public class Tek2465PagingThunkAnalyzer extends AbstractAnalyzer {
 
 	@Override
 	public void optionsChanged(Options options, Program program) {
+		scopeKind = options.getEnum(OPTION_SCOPE_KIND, scopeKind);
 		reprocessThunks = options.getBoolean(OPTION_REPROCESS_THUNKS, reprocessThunks);
 		markPageCallersAsFunctions =
 			options.getBoolean(OPTION_MARK_PAGE_CALLER_FUNCTIONS, markPageCallersAsFunctions);
@@ -98,18 +112,57 @@ public class Tek2465PagingThunkAnalyzer extends AbstractAnalyzer {
 	public boolean added(Program program, AddressSetView set, TaskMonitor monitor, MessageLog log)
 			throws CancelledException {
 		var functions = program.getFunctionManager().getFunctionsOverlapping(set);
-		functions.forEachRemaining(f -> processFunction(f, monitor, log));
+
+		AddressSet toRevisit = new AddressSet();
+		functions.forEachRemaining(f -> {
+			if (processFunction(f, monitor, log)) {
+				toRevisit.add(f.getBody());
+			}
+		});
+
+		// Schedule a rerun over the functions that couldn't be completely processed.
+		if (!toRevisit.isEmpty()) {
+			var manager = AutoAnalysisManager.getAnalysisManager(program);
+			manager.scheduleOneTimeAnalysis(this, toRevisit);
+		}
 
 		return true;
 	}
 
-	private void processFunction(Function f, TaskMonitor monitor,
+	private ScopeKind getScopeKindFromProgram(Program program) {
+		Memory memory = program.getMemory();
+		for (MemoryBlock memoryBlock : memory.getBlocks()) {
+			if (memoryBlock.isInitialized()) {
+				try {
+					ByteProvider byteProvider =
+						MemoryByteProvider.createMemoryBlockByteProvider(memory, memoryBlock);
+					int[] offsets = ROMUtils.findValidRomHeaders(byteProvider);
+
+					for (int offset : offsets) {
+						ROMHeader header = new ROMHeader(byteProvider, offset);
+
+						ScopeKind scopeKind = ROMUtils.scopeKindFromPartNumber(header.partNumber);
+						if (scopeKind != ScopeKind.UNKNOWN) {
+							return scopeKind;
+						}
+					}
+				}
+				catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		return ScopeKind.UNKNOWN;
+	}
+
+	private boolean processFunction(Function f, TaskMonitor monitor,
 			MessageLog log) {
 		if (f.getName().startsWith("PAGE_") && markPageCallersAsFunctions) {
 			markCallersAsFunctions(f, monitor, log);
 		}
 
-		maybeConvertPagingFunctionToThunk(f, monitor, log);
+		return maybeConvertPagingFunctionToThunk(f, monitor, log);
 	}
 
 	private void markCallersAsFunctions(Function f, TaskMonitor monitor, MessageLog log) {
@@ -119,76 +172,59 @@ public class Tek2465PagingThunkAnalyzer extends AbstractAnalyzer {
 
 		for (var ref : referenceManager.getReferencesTo(f.getEntryPoint())) {
 			if (ref.getReferenceType().isCall()) {
-				getOrMarkAsFunction(ref.getFromAddress(), functionManager, log);
+				Address callAddress = ref.getFromAddress();
+				if (scopeKind != ScopeKind.TEK2465A) {
+					// The paging thunks in the early and late Bs start with a DES
+					// instruction.
+					callAddress = callAddress.subtract(1);
+				}
+
+				getOrMarkAsFunction(callAddress, functionManager, log);
 			}
 		}
 	}
 
-	private void maybeConvertPagingFunctionToThunk(Function f, TaskMonitor monitor,
+	/*
+	 * Returns true if this needs to re-run.
+	 */
+	private boolean maybeConvertPagingFunctionToThunk(Function f, TaskMonitor monitor,
 			MessageLog log) {
 		// Don't process functions that are already thunks, unless explicitly set to do so.
 		if (!reprocessThunks && f.isThunk()) {
-			return;
+			return false;
 		}
 
 		// See whether the first instruction is "JSR".
 		Program program = f.getProgram();
 		Listing listing = program.getListing();
-		Instruction pagingCall = listing.getInstructionAt(f.getEntryPoint());
-		if (pagingCall == null || !pagingCall.getMnemonicString().equals("JSR")) {
-			return;
-		}
-		// Lookup the callee function.
-		var refs = pagingCall.getOperandReferences(0);
-		if (refs.length != 1) {
-			return;
-		}
 		FunctionManager functionManager = program.getFunctionManager();
-		Function callee = functionManager.getFunctionAt(refs[0].getToAddress());
-		if (callee == null) {
-			return;
-		}
-		if (!callee.getName().startsWith("PAGE_")) {
-			return;
-		}
 
-		// Resolve the tail of the function name to the destination memory block.
-		String destPage = callee.getName().substring(5);
-		Memory memory = program.getMemory();
-		MemoryBlock block = memory.getBlock(destPage);
-		if (block == null) {
-			return;
+		Address destAddr = getPagingFunctionDestAddr(f, log);
+		if (destAddr == null) {
+			return false;
 		}
-
-		// Construct the address of the fallthrough instruction in the
-		// destination address space.
-		Address destAddr =
-			block.getAddressRange()
-					.getAddressSpace()
-					.getAddress(pagingCall.getFallThrough().getOffset());
 
 		Instruction serviceCall = listing.getInstructionAt(destAddr);
 		if (serviceCall == null) {
 			// Initiate dissassembly at destAddr.
 			DisassembleCommand cmd = new DisassembleCommand(destAddr, null, true);
 			cmd.applyTo(program, monitor);
-			// Just return here as the disassembly is asynchronous.
-			// TODO(siggi): How to schedule a revisit of this function?
-			return;
+			// Just return here as the disassembly is asynchronous, but
+			// ask caller to reschedule visiting this function.
+			return true;
 		}
 
-		// Try again (is this how it works?).
 		serviceCall = listing.getInstructionAt(destAddr);
 		if (serviceCall == null) {
-			return;
+			return false;
 		}
 		if (!serviceCall.getMnemonicString().equals("JSR")) {
-			return;
+			return false;
 		}
 
-		refs = serviceCall.getOperandReferences(0);
+		var refs = serviceCall.getOperandReferences(0);
 		if (refs.length != 1) {
-			return;
+			return false;
 		}
 
 		// Get the service function.
@@ -198,11 +234,60 @@ public class Tek2465PagingThunkAnalyzer extends AbstractAnalyzer {
 			// Success, set the service function.
 			f.setThunkedFunction(service);
 		}
+
+		return false;
+	}
+
+	private Address getPagingFunctionDestAddr(Function f, MessageLog log) {
+		Program program = f.getProgram();
+		Listing listing = program.getListing();
+
+		Address instructionAddr = f.getEntryPoint();
+		// For the Bs, walk over the DES instruction.
+		if (scopeKind != ScopeKind.TEK2465A) {
+			Instruction des = listing.getInstructionAt(instructionAddr);
+			if (des == null || !des.getMnemonicString().equals("DES")) {
+				return null;
+			}
+			instructionAddr = des.getFallThrough();
+		}
+
+		Instruction pagingCall = listing.getInstructionAt(instructionAddr);
+		if (pagingCall == null || !pagingCall.getMnemonicString().equals("JSR")) {
+			return null;
+		}
+		// Lookup the callee function.
+		var refs = pagingCall.getOperandReferences(0);
+		if (refs.length != 1) {
+			return null;
+		}
+		Function callee = program.getFunctionManager().getFunctionAt(refs[0].getToAddress());
+		if (callee == null) {
+			return null;
+		}
+		if (!callee.getName().startsWith("PAGE_")) {
+			return null;
+		}
+
+		// Resolve the tail of the function name to the destination memory block.
+		String destPage = callee.getName().substring(5);
+		Memory memory = program.getMemory();
+		MemoryBlock block = memory.getBlock(destPage);
+		if (block == null) {
+			log.appendMsg("Not a memory page: " + destPage);
+			return null;
+		}
+
+		// Construct the address of the fallthrough instruction in the
+		// destination address space.
+		return block.getAddressRange()
+				.getAddressSpace()
+				.getAddress(pagingCall.getFallThrough().getOffset());
 	}
 
 	private Function getOrMarkAsFunction(Address serviceAddress, FunctionManager functionManager,
 			MessageLog log) {
-		Function function = functionManager.getFunctionAt(serviceAddress);
+		Function function = functionManager.getFunctionContaining(serviceAddress);
 		if (function == null) {
 			try {
 				function = functionManager.createFunction(null, serviceAddress,
@@ -217,6 +302,7 @@ public class Tek2465PagingThunkAnalyzer extends AbstractAnalyzer {
 		return function;
 	}
 
-	boolean reprocessThunks = false;
-	boolean markPageCallersAsFunctions = true;
+	private boolean reprocessThunks = false;
+	private boolean markPageCallersAsFunctions = true;
+	private ScopeKind scopeKind = ScopeKind.UNKNOWN;
 }
