90765ff26c0de3029fcf326900ab9dbb7859c7d0
Sigurdur Asgeirsson <siggi@undo.com>
2024-07-06 08:21:55 -0400

    Fix bad import.

--- src/test/java/is/sort/mc6800/DisassemblyTest.java
+++ src/test/java/is/sort/mc6800/DisassemblyTest.java
@@ -14,9 +14,9 @@
 
 package is.sort.mc6800;
 
-import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.ByteArrayOutputStream;
 
